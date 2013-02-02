399f1062668e2752cbc179ac62f37c4c576a83e5
Phillip Lord <phillip.lord@newcastle.ac.uk>
2013-02-02 17:33:36 +0000

    Moved transform/filter functionality to tawny-owl.

--- src/tawny/obo/go/go.clj
+++ src/tawny/obo/go/go.clj
@@ -1,56 +1,12 @@
 
 
 (ns tawny.obo.go.go
-  (:refer-clojure :only [doall defn str println fn and filter instance? first filter])
+  (:refer-clojure :only [partial])
   (:require [tawny.owl]
             [tawny.read]
             [tawny.memorise])
   (:import (java.io File)
            (org.semanticweb.owlapi.model IRI OWLNamedObject)))
-
-
-
-
-(defn filter-for-go [e]
-  (try
-    ;;(println "Filter:" e)
-    (and (instance? OWLNamedObject e)
-         (.startsWith
-          (.toString (.getIRI e))
-          "http://purl.obolibrary.org/obo/GO"
-          ))
-    (catch Throwable t
-      (println "Problems in filter!: " t)
-      (throw t))))
-
-;; fix the space problem
-(defn transform-for-go [e]
-  (try
-    (clojure.string/replace
-     ;; with luck these will always be literals, so we can do this
-     ;; although not true in general
-     (.getLiteral 
-      ;; get the value of the annotation
-      (.getValue
-       (first
-        ;; filter for annotations which are labels
-        ;; is lazy, so doesn't eval all
-        (doall
-         (filter
-          #(do
-             ;;(println "Loading:" %)
-             (.isLabel (.getProperty %)))
-          ;; get the annotations the big assumption here is that this makes
-          ;; sense. If the entities in question are AnnotationProperties, I am
-          ;; not sure that it is going to work.
-          (.getAnnotations e
-                           (tawny.owl/get-current-ontology)))))))
-     #"[ /]" "_"
-     )
-    (catch Throwable t
-      (println "Problems!: " e)
-      (throw t))
-    ))
 
 
 (tawny.read/defread go
@@ -64,7 +20,10 @@
   :viri "http://purl.obolibrary.org/obo/obi/2012-12-16/go.owl"
   ;; but OBO ontologies are wierd, so pass in a filter function
   :filter
-  filter-for-go
+  (partial tawny.read/iri-starts-with-filter 
+           "http://purl.obolibrary.org/obo/GO")
   :transform
-  transform-for-go
+  tawny.read/exception-nil-label-transform
   )
+
+
